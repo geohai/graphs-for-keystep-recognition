c2183e6b54b36e40bd11c3eb11bc560a59625b61
julialromero <julialromero@gmail.com>
2024-06-07 17:17:15 -0600

    added heterogeneous graph full first implementation

---
+++ gravit/models/context_reasoning/spell_heterogeneous.py
@@ -0,0 +1,218 @@
+import torch
+from torch.nn import Module, ModuleList, Conv1d, Sequential, ReLU, Dropout, functional as F
+from torch_geometric.nn import to_hetero, Linear, EdgeConv, GATv2Conv, SAGEConv, BatchNorm
+import numpy as np
+from torch_geometric.data import HeteroData
+
+
+
+class DilatedResidualLayer(Module):
+    def __init__(self, dilation, in_channels, out_channels):
+        super(DilatedResidualLayer, self).__init__()
+        self.conv_dilated = Conv1d(in_channels, out_channels, kernel_size=3, padding=dilation, dilation=dilation)
+        self.conv_1x1 = Conv1d(out_channels, out_channels, kernel_size=1)
+        self.relu = ReLU()
+        self.dropout = Dropout()
+
+    def forward(self, x):
+        out = self.relu(self.conv_dilated(x))
+        out = self.conv_1x1(out)
+        out = self.dropout(out)
+        return x + out
+
+
+# This is for the iterative refinement (we refer to MSTCN++: https://github.com/sj-li/MS-TCN2)
+class Refinement(Module):
+    def __init__(self, final_dim, num_layers=10, interm_dim=64):
+        super(Refinement, self).__init__()
+        self.conv_1x1 = Conv1d(final_dim, interm_dim, kernel_size=1)
+        self.layers = ModuleList([DilatedResidualLayer(2**i, interm_dim, interm_dim) for i in range(num_layers)])
+        self.conv_out = Conv1d(interm_dim, final_dim, kernel_size=1)
+
+    def forward(self, x):
+        f = self.conv_1x1(x)
+        for layer in self.layers:
+            f = layer(f)
+        out = self.conv_out(f)
+        return out
+    
+
+
+class SPELL_HETEROGENEOUS(Module):
+    def __init__(self, cfg, add_text=True):
+        super(SPELL_HETEROGENEOUS, self).__init__()
+        self.add_text = add_text
+        self.use_spf = cfg['use_spf'] # whether to use the spatial features
+        self.num_modality = cfg['num_modality']
+        if self.use_spf:
+            self.layer_spf = Linear(-1, cfg['proj_dim']) # projection layer for spatial features
+
+        # manually add the first layer (dependent on node input dim)
+        channels = [cfg['channel1'], cfg['channel2']]
+        input_dim = cfg['input_dim'] 
+        
+        self.layer011_omnivore = Linear(input_dim, channels[0]) 
+         
+        # hetero gnn
+        if self.add_text:
+            text_input_dim = 512
+            self.layer011_text = Linear(text_input_dim, channels[0])
+            node_types = ['text', 'omnivore']
+            edge_types = [ ('omnivore', 'to', 'omnivore'), ('omnivore', 'to', 'text')] 
+
+        else:
+            node_types = ['omnivore']
+            edge_types = [ ('omnivore', 'to', 'omnivore')]
+        
+        metadata = (node_types, edge_types)
+        base_spell = SPELL(cfg)
+        self.model = to_hetero(base_spell, metadata, aggr='sum')
+        
+        # print('Model:', self.model)
+
+    def forward(self, data: HeteroData, c=None):
+        if self.add_text == False:
+            data.x_dict = {'omnivore': data.x_dict['omnivore']}
+            data.edge_index_dict = {('omnivore', 'to', 'omnivore'): data.edge_index_dict['omnivore', 'to', 'omnivore']}
+            data.edge_attr_dict = {('omnivore', 'to', 'omnivore'): data.edge_attr_dict['omnivore', 'to', 'omnivore']}
+
+
+        if self.use_spf:
+            feature_dim = data.x_dict['omnivore'].shape[1]
+            x = data.x_dict['omnivore']
+            x_omnivore = self.layer011_omnivore(torch.cat((x[:, :feature_dim//self.num_modality], self.layer_spf(c)), dim=1)) 
+            if 'text' in data.x_dict.keys():
+                feature_dim = data.x_dict['text'].shape[1]
+                x = data.x_dict['text']
+                x_text = self.layer011_text(torch.cat((x[:, :feature_dim//self.num_modality], self.layer_spf(c)), dim=1)) 
+
+        else:
+            feature_dim = data.x_dict['omnivore'].shape[1]
+            x = data.x_dict['omnivore']
+            x_omnivore = self.layer011_omnivore(x[:, :feature_dim//self.num_modality])
+            if 'text' in data.x_dict.keys():
+                feature_dim = data.x_dict['text'].shape[1]
+                x = data.x_dict['text']
+                x_text = self.layer011_text(x[:, :feature_dim//self.num_modality]) 
+
+
+        # print(f'Omnivore input shape: {x_omnivore.shape}')
+        # print(f'Text input shape: {x_text.shape}')
+
+        data['omnivore'].x = x_omnivore
+        if 'text' in data.x_dict.keys():
+            data['text'].x = x_text
+
+        
+        # print('---------')
+        # for key in data.x_dict.keys():
+        #     print(f'Key: {key}, Shape: {data.x_dict[key].shape}')
+        # for key in data.edge_index_dict.keys():
+        #     print(f'Key: {key}, Shape: {data.edge_index_dict[key].shape}')
+        # for key in data.edge_attr_dict.keys():
+        #     print(f'Key: {key}, Shape: {data.edge_attr_dict[key].shape}')
+
+        x_dict = self.model(data.x_dict, data.edge_index_dict, data.edge_attr_dict, c)
+        
+        return x_dict['omnivore']
+        
+
+class SPELL(Module):
+    def __init__(self, cfg):
+        super(SPELL, self).__init__()
+        
+        self.use_ref = cfg['use_ref']
+        self.num_modality = cfg['num_modality']
+
+        channels = [cfg['channel1'], cfg['channel2']]
+        final_dim = cfg['final_dim']
+        
+        num_att_heads = cfg['num_att_heads']
+        dropout = cfg['dropout']
+
+        
+        if self.num_modality == 2:
+            self.layer012 = Linear(-1, channels[0])
+
+        self.batch01 = BatchNorm(channels[0])
+        self.relu = ReLU()
+        self.dropout = Dropout(dropout)
+
+        self.layer11 = EdgeConv(Sequential(Linear(2*channels[0], channels[0]), ReLU(), Linear(channels[0], channels[0])))
+        self.batch11 = BatchNorm(channels[0])
+        self.layer12 = EdgeConv(Sequential(Linear(2*channels[0], channels[0]), ReLU(), Linear(channels[0], channels[0])))
+        self.batch12 = BatchNorm(channels[0])
+        self.layer13 = EdgeConv(Sequential(Linear(2*channels[0], channels[0]), ReLU(), Linear(channels[0], channels[0])))
+        self.batch13 = BatchNorm(channels[0])
+
+        if num_att_heads > 0:
+            self.layer21 = GATv2Conv(channels[0], channels[1], heads=num_att_heads, add_self_loops=False) #DEBUGGING, set add_self_loops to False
+        else:
+            self.layer21 = SAGEConv(channels[0], channels[1])
+            num_att_heads = 1
+        self.batch21 = BatchNorm(channels[1]*num_att_heads)
+
+        self.layer31 = SAGEConv(channels[1]*num_att_heads, final_dim)
+        self.layer32 = SAGEConv(channels[1]*num_att_heads, final_dim)
+        self.layer33 = SAGEConv(channels[1]*num_att_heads, final_dim)
+
+        if self.use_ref:
+            self.layer_ref1 = Refinement(final_dim)
+            self.layer_ref2 = Refinement(final_dim)
+            self.layer_ref3 = Refinement(final_dim)
+
+
+    def forward(self, x, edge_index, edge_attr, c=None):
+        x = self.batch01(x)
+        x = self.relu(x)
+
+        edge_index_f = edge_index[:, edge_attr<=0]
+        edge_index_b = edge_index[:, edge_attr>=0]
+
+
+        ######## Forward-graph stream
+        x1 = self.layer11(x, edge_index_f)
+
+        x1 = self.batch11(x1)
+        x1 = self.relu(x1)
+        x1 = self.dropout(x1)
+        x1 = self.layer21(x1, edge_index_f)
+        x1 = self.batch21(x1)
+        x1 = self.relu(x1)
+        x1 = self.dropout(x1)
+
+        ######## Backward-graph stream
+        x2 = self.layer12(x, edge_index_b)
+        x2 = self.batch12(x2)
+        x2 = self.relu(x2)
+        x2 = self.dropout(x2)
+        x2 = self.layer21(x2, edge_index_b)
+        x2 = self.batch21(x2)
+        x2 = self.relu(x2)
+        x2 = self.dropout(x2)
+
+        ######## Undirected-graph stream
+        x3 = self.layer13(x, edge_index)
+        x3 = self.batch13(x3)
+        x3 = self.relu(x3)
+        x3 = self.dropout(x3)
+        x3 = self.layer21(x3, edge_index)
+        x3 = self.batch21(x3)
+        x3 = self.relu(x3)
+        x3 = self.dropout(x3)
+
+        x1 = self.layer31(x1, edge_index_f)
+        x2 = self.layer32(x2, edge_index_b)
+        x3 = self.layer33(x3, edge_index)
+
+        out = x1+x2+x3
+            
+        
+        if self.use_ref:
+            xr0 = torch.permute(out, (1, 0)).unsqueeze(0)
+            xr1 = self.layer_ref1(torch.softmax(xr0, dim=1))
+            xr2 = self.layer_ref2(torch.softmax(xr1, dim=1))
+            xr3 = self.layer_ref3(torch.softmax(xr2, dim=1))
+            out = torch.stack((xr0, xr1, xr2, xr3), dim=0).squeeze(1).transpose(2, 1).contiguous()
+
+        return out
---
+++ tools/train_context_reasoning_heterogeneous.py
@@ -0,0 +1,148 @@
+import os
+import yaml
+import torch
+import torch.optim as optim
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import pool
+from gravit.utils.parser import get_args, get_cfg
+from gravit.utils.logger import get_logger
+from gravit.models import build_model, get_loss_func
+from gravit.datasets import GraphDataset
+
+from gravit.utils.formatter import get_formatting_data_dict, get_formatted_preds, get_formatted_preds_egoexo_omnivore, get_formatted_preds_framewise
+from gravit.utils.eval_tool import get_eval_score
+
+import numpy as np
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+# Initialize distributed training
+# dist.init_process_group("gloo")
+
+def train(cfg):
+    """
+    Run the training process given the configuration
+    """
+
+    # Input and output paths
+    path_graphs = os.path.join(cfg['root_data'], f'graphs/{cfg["graph_name"]}')
+    if cfg['split'] is not None:
+        path_graphs = os.path.join(path_graphs, f'split{cfg["split"]}')
+    path_result = os.path.join(cfg['root_result'], f'{cfg["exp_name"]}')
+    os.makedirs(path_result, exist_ok=True)
+    print(cfg)
+
+    # Prepare the logger and save the current configuration for future reference
+    logger = get_logger(path_result, file_name='train')
+    logger.info(cfg['exp_name'])
+    logger.info('Saving the configuration file')
+    with open(os.path.join(path_result, 'cfg.yaml'), 'w') as f:
+        yaml.dump({k: v for k, v in cfg.items() if v is not None}, f, default_flow_style=False, sort_keys=False)
+
+    # Build a model and prepare the data loaders
+    logger.info('Preparing a model and data loaders')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    model = build_model(cfg, device)
+    model.to(device)
+
+
+    train_loader = DataLoader(GraphDataset(os.path.join(path_graphs, 'train')), batch_size=cfg['batch_size'], shuffle=True)
+    val_loader = DataLoader(GraphDataset(os.path.join(path_graphs, 'val')))
+   
+    # Prepare the experiment
+    loss_func = get_loss_func(cfg)
+    loss_func_val = get_loss_func(cfg, 'val')
+    optimizer = optim.Adam(model.parameters(), lr=cfg['lr'], weight_decay=cfg['wd'])
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=cfg['sch_param'])
+
+    # Run the training process
+    logger.info('Training process started')
+    print(f'Length of train_loader:', len(train_loader))
+    print(f'Batch size:', cfg['batch_size'])
+
+
+    min_loss_val = float('inf')
+    for epoch in range(1, cfg['num_epoch']+1):
+        print(f'------- Epoch: {epoch} --------')
+        model.train()
+
+        # Train for a single epoch
+        loss_sum = 0
+        # accumulated_gradients = {name: torch.zeros_like(param) for name, param in model.named_parameters()}
+        for data in train_loader:
+            optimizer.zero_grad()
+
+            # print(data.to_dict())
+            # print(data.metadata)
+
+            data = data.to(device)
+
+            y = data.y_dict['omnivore'].to(device)
+
+            if cfg['use_spf']:
+                c = data.c.to(device)
+            else:
+                c = None
+
+            logits = model(data, c)
+            
+            loss = loss_func(logits, y)
+            loss.backward()
+            loss_sum += loss.item()
+            optimizer.step()
+
+
+        # Adjust the learning rate
+        scheduler.step()
+
+        loss_train = loss_sum / len(train_loader)
+
+        # Get the validation loss
+        loss_val = val(val_loader, cfg['use_spf'], model, device, loss_func_val)
+        
+
+        # Save the best-performing checkpoint
+        if loss_val < min_loss_val:
+            min_loss_val = loss_val
+            epoch_best = epoch
+            torch.save(model.state_dict(), os.path.join(path_result, 'ckpt_best.pt'))
+
+        # Log the losses for every epoch
+        logger.info(f'Epoch [{epoch:03d}|{cfg["num_epoch"]:03d}] loss_train: {loss_train:.4f}, loss_val: {loss_val:.4f}, best: epoch {epoch_best:03d}')
+
+    logger.info('Training finished')
+
+
+def val(val_loader, use_spf, model, device, loss_func):
+    """
+    Run a single validation process
+    """
+
+    model.eval()
+    data_dict = get_formatting_data_dict(cfg)
+    loss_sum = 0
+    predictions = []
+    with torch.no_grad():
+        for data in val_loader:  
+            y = data.y_dict['omnivore'].to(device)
+            data = data.to(device)
+            
+            if use_spf:
+                c = data.c.to(device)
+            else:
+                c = None
+                
+            logits = model(data, c)
+
+            loss = loss_func(logits, y)
+            loss_sum += loss.item()
+
+    return loss_sum / len(val_loader)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    cfg = get_cfg(args)
+
+    train(cfg)
